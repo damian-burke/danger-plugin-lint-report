986cc7ea905640332690ad60a47db4bec1ae96df
Jonathan Burke <jonathan.burke@codecentric.de>
2021-08-18 17:17:05 +0200

    test: add test for problematic async/await

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,4 @@
-import { scanXmlReport } from "."
-
+import { scan, scanXmlReport } from "."
 
 const root = "/root/"
 const xmlReport = `
@@ -41,40 +40,85 @@ const xmlReport = `
 </issues>
 `
 
+const mockGlob = jest.fn(() => [] as string[])
+jest.mock("glob", () => (_, cb) => cb(null, mockGlob()))
+jest.mock("fs", () => ({
+  readFileSync: () => `<?xml version="1.0" encoding="UTF-8"?>
+<issues format="5" by="lint 4.2.0-alpha01">
+
+    <issue
+        id="HardcodedText"
+        severity="Warning"
+        message="Hardcoded string &quot;Email Address&quot;, should use \`@string\` resource"
+        category="Internationalization"
+        priority="5"
+        summary="Hardcoded text"
+        explanation="Hardcoding text attributes directly in layout files is bad for several reasons:&#xA;&#xA;* When creating configuration variations (for example for landscape or portrait) you have to repeat the actual text (and keep it up to date when making changes)&#xA;&#xA;* The application cannot be translated to other languages by just adding new translations for existing string resources.&#xA;&#xA;There are quickfixes to automatically extract this hardcoded string into a resource lookup."
+        errorLine1="        android:hint=&quot;Email Address&quot;"
+        errorLine2="        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~">
+        <location
+            file="feature/src/main/res/layout/fragment_password_reset.xml"
+            line="13"
+            column="9"/>
+    </issue>
+
+</issues>`,
+}))
+
+describe.only("scan()", () => {
+  it("scans multiple files and exits after all are finished", async () => {
+    const git = {
+      modified_files: ["feature/src/main/res/layout/fragment_password_reset.xml"],
+      created_files: [],
+      structuredDiffForFile: async () =>
+        new Promise(res => setTimeout(() => res({ chunks: [{ changes: [{ type: "add", ln: 13 }] }] }), 100)),
+    }
+    ;(global as any).danger = { git }
+    ;(global as any).warn = jest.fn()
+
+    mockGlob.mockImplementation(() => ["feature/src/main/res/layout/fragment_password_reset.xml"])
+
+    await scan({
+      fileMask: "",
+      reportSeverity: true,
+      requireLineModification: true,
+    })
+
+    expect((global as any).warn).toHaveBeenCalled()
+  })
+})
 
 describe("scanXmlReport()", () => {
-    it("works across multiple files with require line modification turned off", async () => {
-        const git = {
-            modified_files: [
-                "feature/src/main/res/layout/fragment_password_reset.xml",
-                "feature/src/main/res/layout/fragment_password_reset_2.xml",
-            ],
-            created_files: [],
-        }  
-    
-        const messageCallback = jest.fn()
-    
-        await scanXmlReport(git, xmlReport, root, false, messageCallback)
-    
-        expect(messageCallback).toHaveBeenCalledTimes(2)
-      })
+  it("works across multiple files with require line modification turned off", async () => {
+    const git = {
+      modified_files: [
+        "feature/src/main/res/layout/fragment_password_reset.xml",
+        "feature/src/main/res/layout/fragment_password_reset_2.xml",
+      ],
+      created_files: [],
+    }
 
-      it("returns correct violation data and file path with line modification turned off", async () => {
-        const git = {
-            modified_files: [
-                "feature/src/main/res/layout/fragment_password_reset.xml",
-            ],
-            created_files: [],
-        }  
-    
-        const messageCallback = jest.fn()
-    
-        await scanXmlReport(git, xmlReport, root, false, messageCallback)
-    
-        const msg = "Hardcoded text"
-        const file = "feature/src/main/res/layout/fragment_password_reset.xml"
-        const line = 13
-        const severity = "Warning"
-        expect(messageCallback).toBeCalledWith(msg, file, line, severity)
-      })
+    const messageCallback = jest.fn()
+
+    await scanXmlReport(git, xmlReport, root, false, messageCallback)
+
+    expect(messageCallback).toHaveBeenCalledTimes(2)
+  })
+
+  it("returns correct violation data and file path with line modification turned off", async () => {
+    const git = {
+      modified_files: ["feature/src/main/res/layout/fragment_password_reset.xml"],
+      created_files: [],
+    }
+
+    const messageCallback = jest.fn()
+
+    await scanXmlReport(git, xmlReport, root, false, messageCallback)
+
+    const msg = "Hardcoded text"
+    const file = "feature/src/main/res/layout/fragment_password_reset.xml"
+    const line = 13
+    const severity = "Warning"
+    expect(messageCallback).toBeCalledWith(msg, file, line, severity)
+  })
 })
